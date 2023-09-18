fd88fbf1b325326ac8aec12583d0d681f82eb754
Kevin Green <stuckbykev@gmail.com>
2023-09-18 14:26:30 -0700

    Initial Commit

---
+++ PutPutGolf/PutPutGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  PutPutGolfApp.swift
+//  PutPutGolf
+//
+//  Created by Kevin Green on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PutPutGolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
