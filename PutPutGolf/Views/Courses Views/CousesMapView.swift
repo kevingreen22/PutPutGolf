7163489a4400f56e8df87325d3e425564599a1c7
Kevin Green <stuckbykev@gmail.com>
2023-09-28 11:54:41 -0700

    Linking up VM/combine with MapIconView.

--- PutPutGolf/Views/Courses Views/CousesMapView.swift
+++ PutPutGolf/Views/Courses Views/CousesMapView.swift
@@ -7,31 +7,6 @@
 
 import SwiftUI
 
-public struct CoursesMapInfo {
-    static var iconPlacement: [(RotationDegrees, CGPoint)] = [
-        (.left,iconPositions[0]),
-        (.leftMid,iconPositions[1]),
-        (.rightMid,iconPositions[2]),
-        (.right,iconPositions[3])
-    ]
-    
-    static private var iconPositions: [CGPoint] = [
-        CGPoint(x: 70, y: 500),
-        CGPoint(x: 120, y: 700),
-        CGPoint(x: 300, y: 700),
-        CGPoint(x: 365, y: 500)
-    ]
-    
-    static private var degrees: [RotationDegrees] = [.none,.left,.leftMid,.rightMid,.right]
-    
-    public enum RotationDegrees: Double {
-        case none = 0
-        case left = 27
-        case leftMid = 10.0
-        case rightMid = -10.0
-        case right = -27
-    }
-}
 
 
 struct CoursesMapView: View {
@@ -84,6 +59,33 @@ struct CoursesMapView_Previews: PreviewProvider {
 }
 
 
+
+
+public struct CoursesMapInfo {
+    static var iconPlacement: [(RotationDegrees, CGPoint)] = [
+        (.left,iconPositions[0]),
+        (.leftMid,iconPositions[1]),
+        (.rightMid,iconPositions[2]),
+        (.right,iconPositions[3])
+    ]
+    
+    static private var iconPositions: [CGPoint] = [
+        CGPoint(x: 70, y: 500),
+        CGPoint(x: 120, y: 700),
+        CGPoint(x: 300, y: 700),
+        CGPoint(x: 365, y: 500)
+    ]
+    
+    static private var degrees: [RotationDegrees] = [.none,.left,.leftMid,.rightMid,.right]
+    
+    public enum RotationDegrees: Double {
+        case none = 0
+        case left = 27
+        case leftMid = 10.0
+        case rightMid = -10.0
+        case right = -27
+    }
+}
 
 struct CourseMapBallAndClubIcon: View {
     @EnvironmentObject var vm: CoursesViewModel
