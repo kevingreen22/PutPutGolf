82048e1f55649aa909232a785b8f5ca8e0ae18df
Kevin Green <stuckbykev@gmail.com>
2023-09-28 13:08:29 -0700

    Refactored some struct views to their own files.

---
+++ PutPutGolf/Views/Courses Views/SubViews/CourseMapBallAndClubIcon.swift
@@ -0,0 +1,46 @@
+//
+//  CourseMapBallAndClubIcon.swift
+//  PutPutGolf
+//
+//  Created by Kevin Green on 9/28/23.
+//
+
+import SwiftUI
+
+struct CourseMapBallAndClubIcon: View {
+    @EnvironmentObject var vm: CoursesViewModel
+    @Binding var screenSize: CGSize
+    
+    var body: some View {
+        Circle()
+            .foregroundColor(.blue)
+            .frame(width: 200, height: 200)
+            .background {
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 20, height: 400)
+                    .rotationEffect(.degrees(vm.rotation.rawValue))
+            }
+            .overlay {
+                Text("\(vm.title ?? "Select a Course")")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .frame(width: 300)
+                    .padding()
+                    .background{
+                        Image(systemName: "rectangle.fill")
+                            .resizable()
+                            .foregroundColor(.red)
+                    }
+            }
+            .position(x: screenSize.width/2, y: screenSize.height/4)
+    }
+}
+
+
+struct CourseMapBallAndClubIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseMapBallAndClubIcon(screenSize: .constant(CGSize(width: 400, height: 1000)))
+            .environmentObject(CoursesViewModel(url: nil))
+    }
+}
