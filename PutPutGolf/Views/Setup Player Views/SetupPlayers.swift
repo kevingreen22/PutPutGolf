f17cb28e78a90e3b71698657a22e5c5f4fd866a7
Kevin Green <stuckbykev@gmail.com>
2023-10-18 17:11:29 -0700

    Updated some visuals and fixed some issues with the keyboard "done" button and offset of main view.

--- PutPutGolf/Views/Setup Player Views/SetupPlayers.swift
+++ PutPutGolf/Views/Setup Player Views/SetupPlayers.swift
@@ -10,36 +10,37 @@ import SwiftUI
 struct SetupPlayers: View {
     @EnvironmentObject var navVM: NavigationStore
     @StateObject var vm = SetupPlayerViewModel()
+    @FocusState var focusedTextField
     var course: Course
     var navID: Int = 1
-    
+
     init(_ course: Course) {
         self.course = course
     }
     
     
     var body: some View {
-        VStack {
-            profileImageChooser
-            playerNameTextField
-            Spacer()
-            playerList
+        GeometryReader { _ in
+            ZStack {
+                Color.green.ignoresSafeArea()
+                VStack {
+                    profileImage
+                    playerNameTextField
+                    Spacer()
+                    playerList
+                    letsPuttButton
+                }
+                .animation(.easeInOut, value: vm.playerName)
+                .animation(.easeInOut, value: vm.profileImage)
+                .padding(.top)
+                .onAppear { focusedTextField = true }
+                .navigationTitle("Setup Players")
+                .navigationDestination(for: [Player].self) { players in
+                    ScoreCardView(course: course, players: players)
+                }
+            }
         }
-        
-        // LETS PUTT! button
-        .overlay(alignment: .bottom) {
-            letsPuttButton
-        }
-        
-        .animation(.easeInOut, value: vm.playerName)
-        .animation(.easeInOut, value: vm.profileImage)
-        .padding(.top)
-        .onAppear { vm.focusedTextField = true }
-        .navigationTitle("Setup Players")
-        .navigationDestination(for: [Player].self) { players in
-            ScoreCardView(course: course, players: players)
-        }
-        
+        .ignoresSafeArea(.keyboard) // this coupled with the GeometryReader makes it so the view doesn't move up when the key board is shown.
     }
 }
 
@@ -59,17 +60,20 @@ struct SetupPlayersView_Previews: PreviewProvider {
 
 extension SetupPlayers {
     
-    fileprivate var profileImageChooser: some View {
+    fileprivate var profileImage: some View {
         Image(uiImage: vm.profileImage)
             .resizable()
-            .scaleEffect(0.7)
-            .foregroundColor(.gray)
-            .modifier(EditButtonOverlay())
+            .scaledToFit()
+            .padding()
+            .background { Color.white }
+            .modifier(EditButtonOverlay(size: CGSize(width: 150, height: 150)))
+            .overlay { Circle() .stroke(lineWidth: 3) } // border
+            .frame(width: 150, height: 150)
     }
     
     fileprivate var playerNameTextField: some View {
         TextField("Enter Player Name", text: $vm.playerName)
-            .focused(vm.$focusedTextField)
+            .focused($focusedTextField)
             .font(.largeTitle)
             .multilineTextAlignment(.center)
             .padding()
@@ -77,12 +81,16 @@ extension SetupPlayers {
             .submitLabel(.next)
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
-                    Button("Done") {
-                        vm.focusedTextField = false
+                    Spacer()
+                    Button {
+                        focusedTextField = false
+                    } label: {
+                        Text("Done")
+                            .fontWeight(.semibold)
                     }
                 }
             }
-            .onSubmit { vm.textFieldDidSubmit = true }
+            .onSubmit { vm.textFieldDidSubmit = true; focusedTextField = true }
     }
     
     fileprivate var playerList: some View {
@@ -92,8 +100,12 @@ extension SetupPlayers {
                     profileImageThumb(image: $vm.profileImage)
                     Text("\(player.name.wrappedValue)")
                         .font(.title)
+                        .padding(.leading)
+                    Spacer()
+                    Image(systemName: "line.3.horizontal")
                 }
             }
+            
             .onMove { indexSet, offset in
                 vm.newPlayers.move(fromOffsets: indexSet, toOffset: offset)
             }
@@ -101,19 +113,22 @@ extension SetupPlayers {
                 vm.newPlayers.remove(atOffsets: indexSet)
             }
         }
+        .frame(minHeight: 300)
     }
     
     fileprivate func profileImageThumb(image: Binding<UIImage>) -> some View {
-        Image(uiImage: image.wrappedValue)
-            .foregroundColor(.clear)
-            .frame(width: 50, height: 50)
-            .clipShape(Circle())
-            .padding(3)
-            .overlay { Circle() .stroke(lineWidth: 2) }
-            .onTapGesture {
-                // choose profile photo here
-                
-            }
+        Button {
+            // choose profile photo here
+            
+        } label: {
+            Image(uiImage: image.wrappedValue)
+                .frame(width: 50, height: 50)
+                .background(Color.white)
+                .clipShape(Circle())
+                .padding(3)
+                .overlay { Circle() .stroke(lineWidth: 2) }
+        }
+        .foregroundStyle(Color.primary)
     }
     
     fileprivate var letsPuttButton: some View {
@@ -122,18 +137,10 @@ extension SetupPlayers {
             let players = vm.createPlayers(on: course)
             navVM.path.append(players)
         } label: {
-            VStack {
-                if vm.newPlayers.count > 0 {
-                    Image("plain_ball")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .animation(.easeInOut(duration: 1.0), value: vm.newPlayers)
-                }
-                Text("Let's Putt!")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding()
-            }
+            Text("Let's Putt!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
         }
         .disabled(vm.newPlayers.count < 1)
         .padding(.top)
@@ -146,9 +153,10 @@ extension SetupPlayers {
 
 
 fileprivate struct EditButtonOverlay: ViewModifier {
+    var size: CGSize
+    
     func body(content: Content) -> some View {
         content
-            .frame(width: 150, height: 150)
             .overlay(alignment: .bottom) {
                 Button {
                     // choose profile photo here
@@ -157,18 +165,16 @@ fileprivate struct EditButtonOverlay: ViewModifier {
                     Text("Edit")
                         .font(.title2)
                         .foregroundColor(.white)
-                        .offset(y: 5)
-                        .padding()
+                        .offset(y: -5)
                 }
                 .background(alignment: .bottom) {
-                    Color.black.opacity(0.6)
-                        .frame(width: 150, height: 45)
+                    Color.gray
+                        .frame(width: size.width, height: size.width/4)
                         .blur(radius: 5)
                         .clipped()
                 }
             } // edit button
             .clipShape(Circle())
             .padding(3)
-            .overlay { Circle() .stroke(lineWidth: 2) } // border
     }
 }
