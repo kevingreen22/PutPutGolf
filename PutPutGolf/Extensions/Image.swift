7fb092a5f4e3e1e3e890c4f8b314ead4754b041e
Kevin Green <stuckbykev@gmail.com>
2023-10-19 14:52:28 -0700

    Created extension on Image for a custom init that takes a optional UIImage and a systemName string.

---
+++ PutPutGolf/Extensions/Image.swift
@@ -0,0 +1,24 @@
+//
+//  Image.swift
+//  PutPutGolf
+//
+//  Created by Kevin Green on 10/19/23.
+//
+
+import SwiftUI
+
+public extension Image {
+    
+    /// Initializes an new Image view with the UIImage passed or a system icon.
+    /// - Parameters:
+    ///   - uiimage: An optional UIImage object.
+    ///   - systemName: The name of an SF symbol. Default if "person.fill":
+    init(uiimage: UIImage?, systemName: String = "person.fill") {
+        if let uiimage = uiimage {
+            self.init(uiImage: uiimage)
+        } else {
+            self.init(systemName: systemName)
+        }
+    }
+    
+}
