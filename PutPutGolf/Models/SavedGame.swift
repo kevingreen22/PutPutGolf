7629bfc6d32207813b0ac19b627d7b057227a89b
Kevin Green <stuckbykev@gmail.com>
2023-10-21 13:31:48 -0700

    Shanged the name of ScoreCard model to SavedGame to better represent what its used for.

--- PutPutGolf/Models/SavedGame.swift
+++ PutPutGolf/Models/SavedGame.swift
@@ -1,5 +1,5 @@
 //
-//  ScoreCard.swift
+//  SavedGame.swift
 //  PutPutGolf
 //
 //  Created by Kevin Green on 9/28/23.
@@ -7,8 +7,8 @@
 
 import Foundation
 
-struct ScoreCard: Codable, Equatable, Hashable {
+struct SavedGame: Codable, Equatable, Hashable {
     var id = UUID()
     var course: Course
-    var scores: [Player : [Int]]
+    var players: [Player]
 }
