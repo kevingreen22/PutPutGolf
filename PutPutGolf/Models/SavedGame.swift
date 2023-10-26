b8ef22da7dd62d29c93920b3d8d9674a1258e7c6
Kevin Green <stuckbykev@gmail.com>
2023-10-26 15:03:42 -0700

    Changed the AppStorageKeys to just one key. Added an isCompleted property to SavedGame. And added conformance to RawRepresentable so saving to AppStorage is posible via custom class.

--- PutPutGolf/Models/SavedGame.swift
+++ PutPutGolf/Models/SavedGame.swift
@@ -7,46 +7,43 @@
 
 import Foundation
 
+enum AppStorageKeys: CustomStringConvertible {
+    case savedGames
+    
+    var description: String {
+        switch self {
+        case .savedGames: return "savedGames"
+        }
+    }
+}
+
+
 struct SavedGame: Codable, Equatable, Hashable, Identifiable {
     var id = UUID()
     var course: Course
     var players: [Player]
+    var isCompleted: Bool
     var date: Date = Date()
     var dateString: String {
         self.date.formatted(date: .abbreviated, time: .omitted)
     }
 }
 
-enum AppStorageKeys: String, CustomStringConvertible {
-    case currentGame
-    case allSavedGames
-    
-    var description: String {
-        switch self {
-        case .currentGame: return "currentGame"
-        case .allSavedGames: return "allSavedGames"
-        }
-    }
-}
-
 
 /// Allows SavedGame struct to be saved in AppStorage/SceneStorage
 extension SavedGame: RawRepresentable {
+    
     public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8),
-              let result = try? JSONDecoder().decode(SavedGame.self, from: data)
-        else {
-            return nil
-        }
+        guard let data = rawValue.data(using: .utf8), 
+                let result = try? JSONDecoder().decode(SavedGame.self, from: data)
+        else { return nil }
         self = result
     }
 
     public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self),
-              let result = String(data: data, encoding: .utf8)
-        else {
-            return "[]"
-        }
+        guard let data = try? JSONEncoder().encode(self), 
+                let result = String(data: data, encoding: .utf8) 
+        else { return "" }
         return result
     }
 }
