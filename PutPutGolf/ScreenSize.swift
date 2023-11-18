aabccdaa2e501065ca815e4778ecab758b76db2f
Kevin Green <stuckbykev@gmail.com>
2023-11-18 14:59:57 -0800

    Added view extension and moved SizePreferenceKey struct to a ScreenSize file for completeness and ease of reuse. (even though this app does not use this feature anymore).

--- PutPutGolf/ScreenSize.swift
+++ PutPutGolf/ScreenSize.swift
@@ -1,18 +1,42 @@
 //
 //  ScreenSize.swift
-//  PutPutGolf
-//
 //  Created by Kevin Green on 11/11/23.
 //
 
 import SwiftUI
 
-struct ScreenSize: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public extension View {
+    
+    /// Sets the screen size to a binding.
+    func setScreenSize(_ screenSize: Binding<CGSize>) -> some View {
+        self.modifier(ScreenSize(screenSize: screenSize))
+    }
+    
+}
+
+struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
     }
 }
 
-#Preview {
-    ScreenSize()
+
+struct ScreenSize: ViewModifier {
+    @Binding var screenSize: CGSize
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                GeometryReader {
+                    Color.clear.preference(key: SizePreferenceKey.self, value: $0.size)
+                }
+            )
+            .onPreferenceChange(SizePreferenceKey.self) {
+                screenSize = $0
+            }
+    }
 }
+
+
+
