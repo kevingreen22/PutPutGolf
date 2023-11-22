2d660576fda74fbaff3813aff03abfee3a982d25
Kevin Green <stuckbykev@gmail.com>
2023-11-22 10:36:48 -0800

    Removed iOS 17 mapkit code, till a later date.

--- PutPutGolf/Views/Course Map/SubViews/MyMap.swift
+++ PutPutGolf/Views/Course Map/SubViews/MyMap.swift
@@ -1,5 +1,5 @@
 //
-//  Map.swift
+//  MyMap.swift
 //  PutPutGolf
 //
 //  Created by Kevin Green on 10/6/23.
@@ -10,46 +10,27 @@ import MapKit
 
 struct MyMap: View {
     @EnvironmentObject var coursesVM: CoursesViewModel
-
+    
+    
     var body: some View {
-//        if #available(iOS 17.0, *) {
-//            Map(position: $coursesVM.mapPosition, interactionModes: []) {
-//                ForEach(coursesVM.coursesData) { course in
-//                    Annotation(course.name, coordinate: course.coordinates) {
-//                        CourseAnnotationItem(course: course)
-//                            .foregroundStyle(coursesVM.selectedCourse == course ? Color.clear : Color.gray)
-//                            .scaleEffect(coursesVM.selectedCourse == course ? 1.0 : 0.6)
-//                            .shadow(radius: 10)
-//                            .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1), value: coursesVM.selectedCourse)
-//                            .onTapGesture {
-//                                withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1)) {
-//                                    coursesVM.selectedCourse = course
-//                                }
-//                            }
-//                    }
-//                }
-//            }
-//        } else {
-            // Fallback on earlier versions
-            Map(coordinateRegion: $coursesVM.mapRegion,
-                interactionModes: [.pan, .zoom],
-                annotationItems: coursesVM.coursesData,
-                annotationContent: { course in
-                MapAnnotation(coordinate: course.coordinates) {
-                    CourseAnnotationItem()
-                        .scaleEffect(coursesVM.selectedCourse == course ? 1.0 : 0.7)
-                        .shadow(radius: coursesVM.selectedCourse == course ? 10 : 5)
-                        .grayscale(coursesVM.selectedCourse == course ? 0.0 : 1.0)
-                        .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1), value: coursesVM.selectedCourse)
-                        .onTapGesture {
-                            withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1)) {
-                                coursesVM.selectedCourse = course
-                            }
+        Map(coordinateRegion: $coursesVM.mapRegion,
+            interactionModes: [.pan, .zoom],
+            annotationItems: coursesVM.coursesData,
+            annotationContent: { course in
+            MapAnnotation(coordinate: course.coordinates) {
+                CourseAnnotationItem(course: course)
+                    .scaleEffect(coursesVM.selectedCourse == course ? 1.0 : 0.7)
+                    .shadow(radius: coursesVM.selectedCourse == course ? 10 : 5)
+                    .grayscale(coursesVM.selectedCourse == course ? 0.0 : 1.0)
+                    .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1), value: coursesVM.selectedCourse)
+                    .onTapGesture {
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 1)) {
+                            coursesVM.selectedCourse = course
                         }
-                }
-            })
-            .ignoresSafeArea()
-//        }
+                    }
+            }
+        })
+        .ignoresSafeArea()
     }
 }
 
@@ -58,30 +39,7 @@ struct MyMap_Previews: PreviewProvider {
     
     static var previews: some View {
         MyMap()
-            .environmentObject(CoursesViewModel(dataService: MockDataService(mockData: mockData)))
-        
-        CourseAnnotationItem()
+            .environmentObject(CoursesViewModel(coursesData: mockData.courses))
     }
 }
 
-
-
-
-
-
-struct CourseAnnotationItem: View {
-    var body: some View {
-        Circle()
-            .stroke(.black, lineWidth: 3)
-            .overlay {
-                Image("plain_ball")
-                    .resizable()
-                    .scaledToFill()
-                    .saturation(1.0)
-                    .clipShape(Circle())
-                    .padding(3)
-            }
-            .background(Color.white.clipShape(Circle()))
-            .frame(width: 60, height: 60)
-    }
-}
--- PutPutGolf_Tests/ScoreCardViewModel_Tests.swift
+++ PutPutGolf_Tests/ScoreCardViewModel_Tests.swift
@@ -6,9 +6,12 @@
 //
 
 import XCTest
+@testable import PutPutGolf
+import Combine
 
 final class ScoreCardViewModel_Tests: XCTestCase {
-
+    var cancellables = Set<AnyCancellable>()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,19 +20,57 @@ final class ScoreCardViewModel_Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    @MainActor func test_UnitTestScoreCardViewModel_initTotals_resumingGame() {
+        // Given
+        let mock = MockData.instance
+        
+        // When
+        let isResumingGame = true
+        var vm = ScoreCardViewModel(course: mock.courses.first!, players: mock.players, isResumingGame: isResumingGame)
+        
+        // Then
+        XCTAssertEqual(vm.totals.count, mock.players.count)
+        XCTAssertEqual(vm.finalTotals.count, mock.players.count)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    @MainActor func test_UnitTestScoreCardViewModel_initTotals_newGame() {
+        // Given
+        let mock = MockData.instance
+        for player in mock.players {
+            player.scores = Array(repeating: "", count: mock.courses.first!.holes.count)
+            player.challengeScores = Array(repeating: "", count: mock.courses.first!.challenges.count)
         }
+        
+        // When
+        let isResumingGame = false
+        var vm = ScoreCardViewModel(course: mock.courses.first!, players: mock.players, isResumingGame: isResumingGame)
+        
+        // Then
+        XCTAssertEqual(vm.totals.count, mock.players.count)
+        XCTAssertEqual(vm.finalTotals.count, mock.players.count)
+    }
+    
+    
+    @MainActor func test_UnitTestScoreCardViewModel_setGameIsCompletedTrue() {
+        // Given
+        let mock = MockData.instance
+        var vm = ScoreCardViewModel(course: mock.courses.first!, players: mock.players)
+        
+        // When
+        let expectation = XCTestExpectation()
+        vm.$players
+            .sink { players in
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        // Then
+        wait(for: [expectation], timeout: 5)
+        print(mock.players.first!.scores)
+        print(mock.players.first!.challengeScores)
+        XCTAssertTrue(vm.isGameCompleted)
     }
 
 }
