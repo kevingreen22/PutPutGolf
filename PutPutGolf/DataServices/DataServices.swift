2db11b0ad74a4141391760f17b0948b4a8fd9b5d
Kevin Green <stuckbykev@gmail.com>
2023-12-02 11:07:14 -0800

    Restructured the DataService by adding a struct for the production urls, refactored some methods for fetching and posting, added a post method to the data service protocol,

--- PutPutGolf/DataServices/DataServices.swift
+++ PutPutGolf/DataServices/DataServices.swift
@@ -9,33 +9,40 @@ import SwiftUI
 import Combine
 import Alamofire
 
+struct ProductionURLs {
+    static var get = URL(string: "https://try3rg28fg.execute-api.us-west-1.amazonaws.com/live/courses")!
+    static var post = URL(string: "https://putputgolf.com/post")!
+}
+
 protocol DataServiceProtocol: Equatable {
     func getCourses() -> AnyPublisher<[Course], Error>
     func fetchCourses(handler: @escaping ([Course]?)->Void)
+    func post(course: Course, url: URL?, username: String, password: String)
 }
 
 
 class ProductionDataService: DataServiceProtocol {
     
-    let url: URL
+//    let url: URL
+//    
+//    init(url: URL) {
+//        self.url = url
+//    }
     
-    init(url: URL) {
-        self.url = url
-    }
+    init() { }
     
     func getCourses() -> AnyPublisher<[Course], Error> {
-        AF.request(url, method: .get)
+        AF.request(ProductionURLs.get, method: .get)
             .validate()
             .publishDecodable(type: [Course].self)
             .value()
             .print("\(type(of: self)).\(#function)-courses retrieved...")
             .mapError({ $0 as Error })
             .eraseToAnyPublisher()
-
     }
     
     func fetchCourses(handler: @escaping ([Course]?) -> Void) {
-        AF.request(url, method: .get)
+        AF.request(ProductionURLs.get, method: .get)
             .validate()
             .response { response in
                 switch response.result {
@@ -54,21 +61,20 @@ class ProductionDataService: DataServiceProtocol {
             }
     }
     
-    
-    func setCourses(url: URL, username: String, password: String) {
-        // "https://putputgolf.com/post" ??
+    func post(course: Course, url: URL?, username: String, password: String) {
+        
         let credentials = URLCredential(user: username, password: password, persistence: .forSession)
         
-        let parameters: Parameters = [
-            "category": "Movies", 
-            "genre": "Action"
-        ]
+        let parameters = try? JSONSerialization.jsonObject(with: try JSONEncoder().encode(course), options: []) as? [String: Any]
         
         let headers: HTTPHeaders = [
                     .accept("application/json")
                 ]
         
-        AF.request(url, method: .post, parameters: parameters, headers: headers)
+        AF.request(ProductionURLs.post,
+                   method: .post,
+                   parameters: parameters,
+                   headers: headers)
             .authenticate(with: credentials)
             .response { response in
                 print(response.error ?? "No error for post to alamoFire.")
@@ -76,10 +82,9 @@ class ProductionDataService: DataServiceProtocol {
         
     }
     
-    
     // Equatable
     static func == (lhs: ProductionDataService, rhs: ProductionDataService) -> Bool {
-        return lhs.url == rhs.url
+        return self == self //lhs.url == rhs.url
     }
     
 }
@@ -107,8 +112,12 @@ class MockDataService<T>: DataServiceProtocol where T: MockDataProtocol {
     }
     
     func fetchCourses(handler: @escaping ([Course]?) -> Void) {
-        print("\(type(of: self)).\(#function)-mock courses retrieved")
+        print("\(type(of: self)).\(#function)-mock courses retrieved.")
         handler(mockCourses)
+    }
+    
+    func post(course: Course, url: URL?, username: String, password: String) {
+        print("\(type(of: self)).\(#function)-mock course posted.")
     }
     
     // Equatable
