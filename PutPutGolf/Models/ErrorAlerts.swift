5e6878fdb7d23a1280362a36d7b8df0d60bec3e1
Kevin Green <stuckbykev@gmail.com>
2023-12-02 11:08:06 -0800

    Created and error alert schema for ease of showing errors that get thorwn.

--- PutPutGolf/Models/ErrorAlerts.swift
+++ PutPutGolf/Models/ErrorAlerts.swift
@@ -7,12 +7,82 @@
 
 import SwiftUI
 
-struct ErrorAlerts: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+protocol AppAlert {
+    var title: String { get }
+    var message: String? { get }
+    var buttons: AnyView { get }
+}
+
+extension View {
+    
+    func showErrorAlert<T: AppAlert>(alert: Binding<T?>) -> some View {
+        self
+            .alert(alert.wrappedValue?.title ?? "Error", isPresented: Binding(value: alert)) {
+                alert.wrappedValue?.buttons
+            } message: {
+                if let message = alert.wrappedValue?.message {
+                    Text(message)
+                }
+            }
+
     }
 }
 
-#Preview {
-    ErrorAlerts()
+enum ErrorAlerts: Error, LocalizedError, AppAlert  {
+    case basic(String)
+    case advanced(action: ()->Void)
+    
+    var errorDescription: String? {
+        switch self {
+        case .basic(error: let error): "There was and error: \(error.description)"
+        case .advanced: "Opps, something went really wrong."
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .basic: "Error"
+        case .advanced: "Major Error"
+        }
+    }
+    
+    var message: String? {
+        switch self {
+        case .basic: "Someting went wrong, please try again."
+        case .advanced: nil
+        }
+    }
+    
+    var buttons: AnyView {
+        AnyView(getButtons)
+    }
+    
+    @ViewBuilder var getButtons: some View {
+        switch self {
+        case .basic:
+            Button("Ok") { }
+        case .advanced(action: let action):
+            Button("Ok") { action() }
+        }
+    }
+    
+}
+
+
+
+
+
+
+extension Binding {
+    
+    init<T>(value: Binding<T?>) where Value == Bool {
+        self.init {
+            value.wrappedValue != nil
+        } set: { newValue in
+            if !newValue {
+                value.wrappedValue = nil
+            }
+        }
+    }
+    
 }
