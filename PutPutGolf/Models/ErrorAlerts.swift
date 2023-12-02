85372d74a3eb697ea66563a2940866f0cc4677ee
Kevin Green <stuckbykev@gmail.com>
2023-12-02 10:53:37 -0800

    removed some packages that were not used.

---
+++ PutPutGolf/Models/ErrorAlerts.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorAlerts.swift
+//  PutPutGolf
+//
+//  Created by Kevin Green on 12/2/23.
+//
+
+import SwiftUI
+
+struct ErrorAlerts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorAlerts()
+}
---
+++ PutPutGolf/Views/AddCourseInfo.swift
@@ -0,0 +1,18 @@
+//
+//  AddCourseInfo.swift
+//  PutPutGolf
+//
+//  Created by Kevin Green on 11/24/23.
+//
+
+import SwiftUI
+
+struct AddCourseInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCourseInfo()
+}
