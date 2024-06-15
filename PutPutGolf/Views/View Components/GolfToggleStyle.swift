61012e0793e6e461656cf748096b30d79a868b7c
Kevin Green <stuckbykev@gmail.com>
2024-06-15 16:50:04 -0700

    [Feature] Moved custom toggle style to its own file.

--- PutPutGolf/Views/View Components/GolfToggleStyle.swift
+++ PutPutGolf/Views/View Components/GolfToggleStyle.swift
@@ -7,12 +7,64 @@
 
 import SwiftUI
 
-struct GolfToggleStyle: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct GolfToggleStyle: ToggleStyle {
+    @State private var handleRect: CGRect = .zero
+    
+    func makeBody(configuration: Configuration) -> some View {
+        GeometryReader { proxy in
+            ZStack {
+                Capsule()
+                    .fill(configuration.isOn ? Color.accentColor : Color.gray)
+                    .bordered(shape: Capsule(), color: Color.accentColor, lineWidth: 1)
+                
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "checkmark")
+                            .resizable()
+                            .scaledToFit()
+                            .scaleEffect(0.4)
+                            .foregroundStyle(Color.white)
+                            .opacity(configuration.isOn ? 1 : 0)
+                    }
+                
+                    .overlay(alignment: .trailing) {
+                        Image(systemName: "xmark")
+                            .resizable()
+                            .scaledToFit()
+                            .scaleEffect(0.4)
+                            .foregroundStyle(Color.red)
+                            .opacity(configuration.isOn ? 0 : 1)
+                    }
+                
+                    .overlay(alignment: .center) {
+                        Image("golf_ball")
+                            .resizable()
+                            .scaledToFit()
+                            .shadow(radius: 3)
+                            .padding(.vertical, 2)
+                            .padding(.leading, 2)
+                            .padding(.trailing, 1)
+                            .rectReader($handleRect, in: .local)
+                            .offset(x: configuration.isOn ? proxy.size.width/2-handleRect.size.width/2 : -proxy.size.width/2+handleRect.size.width/2)
+                            .onTapGesture {
+                                configuration.isOn.toggle()
+                            }
+                            .gesture(
+                                DragGesture()
+                                    .onEnded { value in
+                                        configuration.isOn.toggle()
+                                    }
+                            )
+                    }
+                configuration.label
+            }.animation(.easeInOut(duration: 0.15), value: configuration.isOn)
+        }.frame(height: 34)
     }
 }
 
 #Preview {
-    GolfToggleStyle()
+    @State var on: Bool = false
+    return Toggle("", isOn: $on)
+        .toggleStyle(GolfToggleStyle())
+        .frame(width: 70)
+        .scaleEffect(4)
 }
