2a34fa21c297ee046261487ff4168d76cfe0a5f4
Kevin Green <stuckbykev@gmail.com>
2024-06-20 10:52:57 -0700

    updated TODO file

--- PutPutGolf/TODO.swift
+++ PutPutGolf/TODO.swift
@@ -7,8 +7,6 @@
 
 
 
-//  When sliding 3-way-toggle while the cursor is still in a box, the box doesn't clear the entry and possibly doesn't use the entry at all.
-
-
-// Haptics not turning off when switched off in settings view
+/*  When sliding 3-way-toggle while the cursor is still in a box, the box doesn't clear the entry and possibly doesn't use the entry at all.
+*/
 
