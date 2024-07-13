fa01628aa029b46780746c6cd42f2fd8d2279cc8
Kevin Green <stuckbykev@gmail.com>
2024-07-13 13:32:45 -0700

    Created example file for PopoverTips

--- PutPutGolf/Helper Files/PopoverTips.swift
+++ PutPutGolf/Helper Files/PopoverTips.swift
@@ -1,18 +1,64 @@
 //
-//  PopoverTips.swift
-//  PutPutGolf
+//  Tips.swift
 //
-//  Created by Kevin Green on 7/13/24.
+//  Created by Kevin Green on 6/20/24.
 //
+// EXAMPLE FILE
 
 import SwiftUI
+import TipKit
 
-struct PopoverTips: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@available(iOS, introduced: 17.0)
+struct AppTips {
+    
+    struct ExampleTip: Tip {
+        static var someEvent = Event(id: "someEvent")
+        static var visitedView = Event(id: "visited")
+        
+        var title: Text {
+            Text("Example Tip")
+        }
+        
+        var message: Text?
+        
+        var image: Image?
+        
+        var rules: [Rule] {
+            #Rule(Self.someEvent) { event in
+                event.donations.count == 0
+            }
+            #Rule(Self.visitedView) { event in
+                event.donations.count > 2
+            }
+        }
     }
+    
+    static var exampleTip = ExampleTip()
+    
 }
 
+
+
+@available(iOS 17.0, *)
 #Preview {
-    PopoverTips()
+    Rectangle()
+        .fill(Color.indigo)
+        .frame(width: 200, height: 200)
+        .popoverTip(AppTips.exampleTip)
+        .task {
+            await AppTips.ExampleTip.someEvent.donate() // <-- increments the "donation" amount for the tips specific rule event.
+        }
+        .onAppear {
+            Task {
+                await AppTips.ExampleTip.visitedView.donate() // <-- increments the "donation" amount for the tips specific rule event.
+            }
+
+        }
+        .task {
+            try? Tips.resetDatastore()
+            try? Tips.configure([
+                .datastoreLocation(.applicationDefault)
+            ])
+        }
 }
+
