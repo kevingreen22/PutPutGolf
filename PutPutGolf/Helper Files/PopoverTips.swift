acdeebd4b8b5852b7bea939e8958e6b1dbdbe643
Kevin Green <stuckbykev@gmail.com>
2024-07-13 13:31:17 -0700

    Created example file of Popover tips

---
+++ PutPutGolf/Helper Files/PopoverTips.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverTips.swift
+//  PutPutGolf
+//
+//  Created by Kevin Green on 7/13/24.
+//
+
+import SwiftUI
+
+struct PopoverTips: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopoverTips()
+}
